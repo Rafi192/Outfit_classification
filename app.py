cbcb6b2d5a1f348ec54dd6f559b540a94ae91279
Rafi192 <rafihasanmmsc@gmail.com>
2025-10-21 11:23:56 +0600

    Commit of procfile

--- app.py
+++ app.py
@@ -44,9 +44,5 @@ def classify_outfit():
     }), 200
 
 
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0',debug=True)
-
-
-
+if __name__ == "__main__":
+    app.run(debug=False, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
