6ca7a14695dd990d8a30a641bfe610b76233b49e
Rafi192 <rafihasanmmsc@gmail.com>
2025-10-21 10:13:34 +0600

    Commit of outfit api

---
+++ app.py
@@ -0,0 +1,46 @@
+from flask import Flask, request, jsonify
+from ultralytics import YOLO
+from PIL import Image
+import os
+import io
+
+# Initialize Flask app
+app = Flask(__name__, template_folder=os.path.join(os.path.dirname(__file__), '..', 'templates'))
+
+# Load YOLOv11 model
+model_path = os.path.join(os.path.dirname(__file__),'..', 'models', 'best.pt')
+model = YOLO(model_path)
+print("Model loaded successfully.", model_path)
+print("Model details:", model)
+
+@app.route('/api/classify_outfit', methods=['GET','POST'])
+def classify_outfit():
+    if 'image' not in request.files:
+        return jsonify({"error": "No file uploaded"}), 400
+    
+    file = request.files['image']
+    image = Image.open(io.BytesIO(file.read())).convert("RGB")
+
+    # Predciton using YOLO model
+    results = model(image)
+    probs = results[0].probs  # class probabilities
+    top_class_id = probs.top1
+    top_class_name = results[0].names[top_class_id]
+    confidence = float(probs.top1conf)
+
+
+    return jsonify({
+        "status": "success",
+        "top_prediction": {
+            "class": top_class_name,
+            "confidence": round(confidence, 3)
+        }
+    }), 200
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
